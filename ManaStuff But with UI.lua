5b01b5db6702442c5f163016feca3bd775777a98
plytalent <39195826+plytalent@users.noreply.github.com>
2022-06-28 23:16:34 +0700

    Update ManaStuff But with UI.lua

--- ManaStuff But with UI.lua
+++ ManaStuff But with UI.lua
@@ -1,8 +1,12 @@
 local Config = {
-    WindowName = "RubyTheSilent Hub [Universal]",
+	WindowName = "RubyTheSilent Hub [Universal]",
 	Color = Color3.fromRGB(255,128,64),
 	Keybind = Enum.KeyCode.RightShift
 }
+if game.PlaceId == 8619263259 then
+	Config.WindowName = "RubyTheSilent Hub [Critical Legends]"
+end
+
 
 local Label_config = {
 	ManaSoul = "Mana Soul",
@@ -12,10 +16,26 @@ local Label_config = {
 	Player = "Player"
 }
 
+if isfile then
+	if isfile("ManaStuff UI Labels.properties") then
+		local content = readfile("ManaStuff UI Labels.properties")
+		if content ~= "" then
+			local lines = content:split("\n")
+			for i=1,#lines do
+				local index_and_value = lines[i]:split("=")
+				Label_config[index_and_value[1]] = index_and_value[2]
+			end
+		end
+	end
+end
+
 local Playerobj = game:GetService("Players").LocalPlayer
 local RunService = game:GetService("RunService")
 local UIS = game:GetService("UserInputService")
-local manaflypos = nil
+local vu = game:GetService("VirtualUser")
+local cf_character_update = CFrame.new()
+local manarunspeed = 2.75
+local manaflyspeed = 2.75
 --==================================[[AFHB]]==================================--
 local frame = 1 / 60
 local allowframeloss = false
@@ -25,7 +45,7 @@ local AFHB_Module = function()
 	local tf = 0	
 	local lastframe = tick()
 	local lasttick = tick()
-	game:GetService("RunService").Stepped:Connect(function()
+	RunService.Stepped:Connect(function()
 	    local rounddelta = tick() - lasttick
 	    lasttick = tick()
 		tf = tf + rounddelta
@@ -49,7 +69,7 @@ local AFHB_Module = function()
 			end
 		end
 	end)
-	game:GetService("RunService").RenderStepped:Connect(function()
+	RunService.RenderStepped:Connect(function()
 	    local rounddelta = tick() - lasttick
 	    lasttick = tick()
 		tf = tf + rounddelta
@@ -73,7 +93,7 @@ local AFHB_Module = function()
 			end
 		end
 	end)
-	game:GetService("RunService").Heartbeat:Connect(function()
+	RunService.Heartbeat:Connect(function()
 	    local rounddelta = tick() - lasttick
 	    lasttick = tick()
 		tf = tf + rounddelta
@@ -104,78 +124,93 @@ end
 
 function fill_decimal(string_decimal)
 	local numbers = string_decimal:split(".")
-	if numbers[2]:len() ~= 2 then
-		if numbers[2]:len() > 2 then
-			numbers[2] = numbers[2]:sub(1,2)
-		else
-			local newdecimal = ""
-			while numbers[2]:len() < 2 do
-				newdecimal = newdecimal .. numbers[2] .. "0"
+	if #numbers == 2 then
+		if numbers[2]:len() ~= 2 then
+			if numbers[2]:len() > 2 then
+				numbers[2] = numbers[2]:sub(1,2)
+			else
+				local newdecimal = ""
+				while numbers[2]:len() < 2 do
+					newdecimal = newdecimal .. numbers[2] .. "0"
+				end
 			end
 		end
+		return numbers[1] .. "." .. numbers[2]
 	end
-	return numbers[1] .. "." .. numbers[2]
-
+	return "00.00"
 end
 
 local ArtificialHB = AFHB_Module()
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Roblox/main/BracketV3.lua"))()
 local Window = Library:CreateWindow(Config, game:GetService("CoreGui"))
+Window:Toggle(false)
 
 local Tab1 = Window:CreateTab("Main")
 local CEV = Window:CreateTab("Artificial Heartbeat Settings")
 local Tab2 = Window:CreateTab("UI Settings")
-
 local movement = Tab1:CreateSection(Label_config.Movement)
 local player = Tab1:CreateSection(Label_config.Player)
-
 local Event = CEV:CreateSection("Artificial Heartbeat")
-
 local UI_1 = Tab2:CreateSection("Menu")
 local UI_2 = Tab2:CreateSection("Background")
 
-local manarunspeed = 2.75
-local manaflyspeed = 2.75
-
-local ManaRun = movement:CreateToggle(Label_config.ManaRun, false, function(State)
-end)
-local ManaFly = movement:CreateToggle(Label_config.ManaFly, false, function(State)
+local ManaRun = movement:CreateToggle(Label_config.ManaRun, false, function()
 	if Playerobj.Character then
-		manaflypos = Playerobj.Character:GetPrimaryPartCFrame()
+		cf_character_update = Playerobj.Character:GetPrimaryPartCFrame()
 	end
 end)
-local ManaSoul = player:CreateToggle(Label_config.ManaSoul, false, function(State)
+local ManaFly = movement:CreateToggle(Label_config.ManaFly, false, function()
+	if Playerobj.Character then
+		cf_character_update = Playerobj.Character:GetPrimaryPartCFrame()
+	end
 end)
-Window:Toggle(false)
+local ManaSoul = player:CreateToggle(Label_config.ManaSoul, false, function()end)
+local Anti_AFK = player:CreateToggle("Anti-AFK", true, function()end)
+Playerobj.Idled:Connect(function()
+	if Anti_AFK:GetState() then
+    	vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		printconsole("Mouse Hold Down!")
+		local starttick = tick()
+		local current_tick = 0
+    	while current_tick >= 1 do
+			current_tick = tick() - starttick
+			ArtificialHB.Event:Wait()
+		end
+    	vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		printconsole("Mouse Released!")
+	end
+end)
+ManaSoul:CreateKeybind(Enum.KeyCode.P.Name)
+ManaFly:CreateKeybind(Enum.KeyCode.K.Name)
+ManaRun:CreateKeybind(Enum.KeyCode.M.Name)
 local Toggle4 = UI_1:CreateToggle("UI Toggle", nil, function(State)
 	Window:Toggle(State)
 end)
 local Toggle7 = UI_1:CreateToggle("TASTE THE RAINBOW", nil, function(State)
+	if not State then
+		Window:ChangeColor(Config.Color)
+	end
 end)
 Toggle4:CreateKeybind(Config.Keybind.Name, function(Key)
 	Config.Keybind = Enum.KeyCode[Key]
 end)
-
-local Slider1 = movement:CreateSlider(Label_config.ManaRun.." Multiplier", 0, 100, manarunspeed, false, function(x)
-	manarunspeed = (x/100)*30
+local Slider1 = movement:CreateSlider(Label_config.ManaRun.." Multiplier", 0, 300, 150, false, function(x)
+	manarunspeed = (x/100)
 	if x == 0 then
 		manarunspeed = 0
 	end
 end)
-
-local Slider2 = movement:CreateSlider(Label_config.ManaFly.." Multiplier", 0, 100, manaflyspeed, false, function(x)
-	manaflyspeed = (x/100)*30
+local Slider2 = movement:CreateSlider(Label_config.ManaFly.." Multiplier", 0, 300, 150, false, function(x)
+	manaflyspeed = (x/100)
 	if x == 0 then
 		manaflyspeed = 0
 	end
 end)
-
 local Colorpicker3 = UI_1:CreateColorpicker("UI Color", function(Color)
 	Config.Color = Color
 	Window:ChangeColor(Color)
 end)
-
 -- credits to jan for patterns
 local Dropdown3 = UI_2:CreateDropdown("Image", {"Default","Hearts","Abstract","Hexagon","Circles","Lace With Flowers","Floral"}, function(Name)
 	if Name == "Default" then
@@ -194,7 +229,7 @@ local Dropdown3 = UI_2:CreateDropdown("Image", {"Default","Hearts","Abstract","H
 		Window:SetBackground("5553946656")
 	end
 end)
-
+local Warning = Event:CreateLabel("WARN: DO NOT ENABLE Toss Remainder")
 local Label1 = Event:CreateLabel("Current Event Frame")
 local Label2 = Event:CreateLabel("Current Event Frame Time: -NaN(%ind) ms")
 local Slider5 = Event:CreateSlider("Event Frame Time",1,200,60,false, function(Value)
@@ -217,7 +252,8 @@ end)
 local Slider4 = UI_2:CreateSlider("Tile Scale",0,1,0.5,false, function(Value)
 	Window:SetTileScale(Value)
 end)
-game:GetService("RunService").Stepped:Connect(function()
+
+RunService.Stepped:Connect(function()
 	if ManaSoul:GetState() then
 		local Character = Playerobj.Character
         if Character then
@@ -229,12 +265,27 @@ game:GetService("RunService").Stepped:Connect(function()
         end
 	end
 end)
-game:GetService("RunService"):BindToRenderStep("Update UI",1,function()
+
+RunService:BindToRenderStep("Update UI",1,function()
 	if Toggle7:GetState() then
 		Window:ChangeColor(Color3.fromHSV(tick() * 128 % 255/255, 1, 0.75))
 		Window:SetBackgroundColor(Color3.fromHSV(tick() * 128 % 255/255, 1, 0.5))
-	else
+	elseif Toggle7:GetState() == false then
 		Window:ChangeColor(Config.Color)
+	end
+	if ManaFly:GetState() then
+		local Character = Playerobj.Character
+		if game.PlaceId == 4691401390 then
+			if workspace:FindFirstChild'Players' then
+				Character = workspace.Players:FindFirstChild(Playerobj.Name);
+			end
+		end
+        if Character ~= nil then
+			if Character.PrimaryPart then
+            	Character:SetPrimaryPartCFrame(cf_character_update)
+				Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+			end
+		end
 	end
 end)
 ArtificialHB.Event:Connect(function(d)
@@ -247,26 +298,25 @@ ArtificialHB.Event:Connect(function(d)
 			end
 		end
         if Character ~= nil then
-			local leftorright,backorforward = 0,0
-			if UIS:IsKeyDown(Enum.KeyCode.S) then
-				backorforward = backorforward + manaflyspeed
+			if Character:FindFirstChild("HumanoidRootPart") then
+				local leftorright,backorforward = 0,0
+				if UIS:IsKeyDown(Enum.KeyCode.S) then
+					backorforward = backorforward + manaflyspeed
+				end
+				if UIS:IsKeyDown(Enum.KeyCode.W) then
+					backorforward = backorforward - manaflyspeed
+				end
+				if UIS:IsKeyDown(Enum.KeyCode.D) then
+					leftorright = leftorright + manaflyspeed
+				end
+				if UIS:IsKeyDown(Enum.KeyCode.A) then
+					leftorright = leftorright - manaflyspeed
+				end
+				local targetCF = workspace.CurrentCamera.CFrame
+				local rx,ry,_ = targetCF:ToOrientation()
+				targetCF = CFrame.new(0,0,0)*CFrame.Angles(0,ry,0) *CFrame.Angles(rx,0,0)
+            	cf_character_update = (CFrame.new(cf_character_update.Position) * targetCF)*CFrame.new(leftorright,0,backorforward)
 			end
-			if UIS:IsKeyDown(Enum.KeyCode.W) then
-				backorforward = backorforward - manaflyspeed
-			end
-			if UIS:IsKeyDown(Enum.KeyCode.D) then
-				leftorright = leftorright + manaflyspeed
-			end
-			if UIS:IsKeyDown(Enum.KeyCode.A) then
-				leftorright = leftorright - manaflyspeed
-			end
-
-			local targetCF = workspace.CurrentCamera.CFrame
-			local rx,ry,_ = targetCF:ToOrientation()
-			targetCF = CFrame.new(0,0,0)*CFrame.Angles(0,ry,0) *CFrame.Angles(rx,0,0)
-            manaflypos = (CFrame.new(manaflypos.p) * targetCF)*CFrame.new(leftorright,0,backorforward)
-            Character:SetPrimaryPartCFrame(manaflypos)
-            Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
 		end
 	elseif ManaRun:GetState() then
 		local Character = Playerobj.Character
@@ -289,18 +339,116 @@ ArtificialHB.Event:Connect(function(d)
         	if UIS:IsKeyDown(Enum.KeyCode.A) then
         	    leftorright = leftorright - manarunspeed
         	end
-			targetCF = workspace.CurrentCamera.CFrame
+			local targetCF = workspace.CurrentCamera.CFrame
 			local _, ry, _ = targetCF:ToOrientation()
-			targetCF = (CFrame.new(Character:GetPrimaryPartCFrame().p)*CFrame.Angles(0,ry,0)) * CFrame.new(leftorright,0,backorforward)
-			Character:SetPrimaryPartCFrame(targetCF)
+			if Character.PrimaryPart then
+				Character:SetPrimaryPartCFrame((CFrame.new(Character:GetPrimaryPartCFrame().Position)*CFrame.Angles(0,ry,0)) * CFrame.new(leftorright,0,backorforward))
+			end
 		end
 	end
 end)
 
+function tablefind(tb,value)
+	for _,v in pairs(tb) do
+		if v == value then
+			return true
+		end
+	end
+	return false
+end
+if game.PlaceId == 8619263259 then
+	local target = nil
+	local targets = {}
+	local safe_offset_cf = CFrame.new(0, 85, 0)
+	for _,zone in pairs(workspace.Enemies:GetChildren()) do
+		for _, enemy in pairs(zone:GetChildren()) do
+			if enemy:IsA("Model") and enemy:FindFirstChild("Model") then
+				if not tablefind(targets,enemy.Model.Value.Name) then
+					targets[#targets+1] = enemy.Model.Value.Name
+				end
+			end
+		end
+	end
+	table.sort(targets)
+	local Critical_Legends_Tab = Window:CreateTab("Critical Legends")
+	local AutoFarmSection = Critical_Legends_Tab:CreateSection("Auto Farm")
+	local TargetHealth = AutoFarmSection:CreateLabel("Target Health: 0/0")
+	local Slider3 = UI_2:CreateSlider("Offset",0,100,0,false, function(Value)
+		safe_offset_cf = CFrame.new(0, Value, 0)
+	end)
+	local AutoFarm_Toggle = AutoFarmSection:CreateToggle("AutoFarm", false,function()end)
+	local AutoOrb_Toggle = AutoFarmSection:CreateToggle("AutoOrb", false,function()end)
+	local Dropdown = AutoFarmSection:CreateDropdown("Targets", targets, function(Name)
+		for _, model in pairs(workspace.Enemies:GetDescendants()) do
+			if model:IsA("ObjectValue") then
+				if model.Value.Name == Name then
+					target = model.Parent
+				end
+			end
+		end
+	end)
+	ArtificialHB.Event:Connect(function()
+		if AutoOrb_Toggle:GetState() or AutoFarm_Toggle:GetState() then
+			if workspace:FindFirstChild("CombatFolder") then
+				if workspace.CombatFolder:FindFirstChild(Playerobj.Name) then
+					if Playerobj.Character:FindFirstChild("Combat Border") then
+						Playerobj.Character["Combat Border"]:Destroy()
+					end
+					local models = workspace.CombatFolder[Playerobj.Name]:GetChildren()
+					if #models > 0 then
+						for i=1, 128 do
+							coroutine.resume(coroutine.create(function()
+								local index = i
+								local s,err = pcall(function()
+									if models[index] then
+										if models[index]:FindFirstChild("Base") then
+											local part = models[index].Base
+											firetouchinterest(part,Playerobj.Character.HumanoidRootPart,1)
+											firetouchinterest(part,Playerobj.Character.HumanoidRootPart,0)
+										end
+									end
+								end)
+								if not s then
+									rconsoleerror(err)
+								end
+							end))
+						end
+					end
+				end
+			end
+		end
+		if AutoFarm_Toggle:GetState() then
+			if target then
+				local CBF = workspace:FindFirstChild("CombatFolder")
+				if CBF then
+					if CBF:FindFirstChild("Owner") then
+						if CBF.Owner.Value then
+							if CBF.Owner.Value:FindFirstChild("Health") then
+								TargetHealth:UpdateText(string.format("Target Health: %.2f/%.2f", CBF.Owner.Value.Health.Value, CBF.Owner.Value.MaxHealth.Value))
+							end
+						end
+					end
+					if CBF:FindFirstChild(Playerobj.Name) then
+						Playerobj.Character.HumanoidRootPart.CFrame = target.EnemyLocation.CFrame * safe_offset_cf
+						Playerobj.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+						Playerobj.Character.HumanoidRootPart.Anchored = false
+					else
+						Playerobj.Character.HumanoidRootPart.CFrame = target.EnemyLocation.CFrame * CFrame.new(0,10,0)
+						Playerobj.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+						Playerobj.Character.HumanoidRootPart.Anchored = false
+					end
+				else
+					Playerobj.Character.HumanoidRootPart.CFrame = target.EnemyLocation.CFrame * CFrame.new(0,10,0)
+					Playerobj.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+					Playerobj.Character.HumanoidRootPart.Anchored = false
+				end
+			end
+		end
+	end)
+end
 Colorpicker3:UpdateColor(Config.Color)
-Dropdown3:SetOption("Default")
+Dropdown3:SetOption("Abstract")
 Colorpicker4:UpdateColor(Color3.new(1,1,1))
-
 ArtificialHB:Fire(0)
 ArtificialHB.Event:Wait()
 Toggle4:SetState(true)
