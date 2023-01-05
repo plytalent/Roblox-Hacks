1730c7cda6ff1ee21382510d725b78d65dd52227
plytalent <39195826+plytalent@users.noreply.github.com>
2023-01-05 12:40:42 +0700

    Create Unnamed ESP Patcher.lua

---
+++ Unnamed ESP Patcher.lua
@@ -0,0 +1,12 @@
+printconsole("Fetching UnnamedESP Source Code")
+local source_code = game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua')
+printconsole("Find local Variable 'Modules' End Array UnnamedESP Source Code")
+local source_start_injection_code, source_end_injection_code = source_code:find('};\n\nif')
+local newmodules = game:HttpGet('https://github.com/plytalent/Roblox-Hacks/raw/main/UnnamedESP%20Patch%20For%20Patcher/NewModule.lua')
+local replacemodules = game:HttpGet('https://github.com/plytalent/Roblox-Hacks/raw/main/UnnamedESP%20Patch%20For%20Patcher/ReplaceModule.lua')
+printconsole("Executing Modified UnnamedESP Source Code")
+local newsourcecode = source_code:sub(1,source_start_injection_code-1)..";\n--[[Start of New Modules]]--\n" .. newmodules .. "\n--[[End of New Modules]]--\n};\n\n--[[Start of Replace Modules]]--\n" .. replacemodules..  "\n--[[End of Replace Modules]]--\n".. source_code:sub(source_end_injection_code-1)
+local s,err = pcall(loadstring(newsourcecode))
+if not s then
+    printconsole("[UnnamedESP][ERROR]"..err)
+end
