d61feaccefce5446cb561296bdeaf3065359b95a
plytalent <39195826+plytalent@users.noreply.github.com>
2025-03-25 19:53:34 +0700

    Update Fluent ManaStuff.lua

--- Fluent ManaStuff.lua
+++ Fluent ManaStuff.lua
@@ -133,8 +133,10 @@ local Window = Fluent:CreateWindow({
 local Tabs = {
     Main = Window:AddTab({ Title = "Movements", Icon = "chevrons-up" }),
     Keybinds = Window:AddTab({ Title = "Keybinds", Icon = "keyboard" }),
+    Misc = Window:AddTab({ Title = "Misc", Icon = "circle-ellipsis" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
-    Debug = Window:AddTab({ Title = "Debug", Icon = "bug" })
+    Debug = Window:AddTab({ Title = "Debug", Icon = "bug" }),
+    Funny = Window:AddTab({ Title = "Funny", Icon = "party-popper" })
 }
 
 local movement_dir_z, movement_dir_x = 0, 0
@@ -453,3 +455,195 @@ else
     })
 end
 Window:SelectTab(1)
+if not Character:FindFirstChild("Torso") then
+    Tabs.Funny:AddParagraph({
+        Title = "CustomAnimation",
+        Content = "Incompatible BodyType"
+    })
+else
+    Tabs.Funny:AddParagraph({
+        Title = "CustomAnimation",
+        Content = "!!!WARNING!!!! Use At Your Own Risk!!!"
+    })
+    local CustomAnimation_toggle = Tabs.Funny:AddToggle("CustomAnimation", {
+        Title = "Custom Animation Fly/Run",
+        Default = false
+    })
+    env.CustomAnimation = false
+    CustomAnimation_toggle:OnChanged(function()
+        env.CustomAnimation = Options.CustomAnimation.Value
+    end)
+    function QuaternionSlerp(a, b, t)
+        local cosTheta = a[1] * b[1] + a[2] * b[2] + a[3] * b[3] + a[4] * b[4]
+        local startInterp, finishInterp;
+        if cosTheta >= 0.0001 then
+            if (1 - cosTheta) > 0.0001 then
+                local theta = math.acos(cosTheta)
+                local invSinTheta = 1 / math.sin(theta)
+                startInterp = math.sin((1 - t) * theta) * invSinTheta
+                finishInterp = math.sin(t * theta) * invSinTheta
+            else
+                startInterp = 1 - t
+                finishInterp = t
+            end
+        else
+            if (1 + cosTheta) > 0.0001 then
+                local theta = math.acos(-cosTheta)
+                local invSinTheta = 1 / math.sin(theta)
+                startInterp = math.sin((t - 1) * theta) * invSinTheta
+                finishInterp = math.sin(t * theta) * invSinTheta
+            else
+                startInterp = t - 1
+                finishInterp = t
+            end
+        end
+        return a[1] * startInterp + b[1] * finishInterp, a[2] * startInterp + b[2] * finishInterp, a[3] * startInterp + b[3] * finishInterp, a[4] * startInterp + b[4] * finishInterp
+    end
+
+    function QuaternionFromCFrame(cf) 
+        local mx, my, mz, m00, m01, m02, m10, m11, m12, m20, m21, m22 = cf:components() 
+        local trace = m00 + m11 + m22 
+        if trace > 0 then 
+            local s = math.sqrt(1 + trace) 
+            local recip = 0.5/s 
+            return (m21-m12)*recip, (m02-m20)*recip, (m10-m01)*recip, s*0.5 
+        else 
+            local i = 0 
+            if m11 > m00 then
+                i = 1
+            end
+            if m22 > (i == 0 and m00 or m11) then 
+                i = 2 
+            end 
+            if i == 0 then 
+                local s = math.sqrt(m00-m11-m22+1) 
+                local recip = 0.5/s 
+                return 0.5*s, (m10+m01)*recip, (m20+m02)*recip, (m21-m12)*recip 
+            elseif i == 1 then 
+                local s = math.sqrt(m11-m22-m00+1) 
+                local recip = 0.5/s 
+                return (m01+m10)*recip, 0.5*s, (m21+m12)*recip, (m02-m20)*recip 
+            elseif i == 2 then 
+                local s = math.sqrt(m22-m00-m11+1) 
+                local recip = 0.5/s return (m02+m20)*recip, (m12+m21)*recip, 0.5*s, (m10-m01)*recip 
+            end 
+        end 
+    end
+    function QuaternionToCFrame(px, py, pz, x, y, z, w) 
+        local xs, ys, zs = x + x, y + y, z + z 
+        local wx, wy, wz = w*xs, w*ys, w*zs 
+        local xx = x*xs 
+        local xy = x*ys 
+        local xz = x*zs 
+        local yy = y*ys 
+        local yz = y*zs 
+        local zz = z*zs 
+        return CFrame.new(px, py, pz,1-(yy+zz), xy - wz, xz + wy,xy + wz, 1-(xx+zz), yz - wx, xz - wy, yz + wx, 1-(xx+yy)) 
+    end
+
+    function Clerp(a,b,t) 
+        local qa = {QuaternionFromCFrame(a)}
+        local qb = {QuaternionFromCFrame(b)} 
+        local ax, ay, az = a.x, a.y, a.z 
+        local bx, by, bz = b.x, b.y, b.z
+        local _t = 1-t
+        return QuaternionToCFrame(_t*ax + t*bx, _t*ay + t*by, _t*az + t*bz,QuaternionSlerp(qa, qb, t)) 
+    end
+    function rayCast(Pos, Dir, Max, Ignore)  -- Origin Position , Direction, MaxDistance , IgnoreDescendants
+        return workspace:FindPartOnRay(Ray.new(Pos, Dir.unit * (Max or math.huge)), Ignore) 
+    end 
+    local Links = {}
+    local Torso = Character.Torso
+    local RHI = Torso["Right Hip"]
+    local LHI = Torso["Left Hip"]
+    local RSH = Torso["Right Shoulder"]
+    local LSH = Torso["Left Shoulder"]
+    local RootJoint = rootpart.RootJoint
+    local RootCF=CFrame.fromEulerAnglesXYZ(-1.57,0,3.14)
+    local necko=CFrame.new(0, 1, 0, -1, -0, -0, 0, 0, 1, 0, 1, 0)
+    if env.animation_ev then
+        env.animation_ev:Disconnect()
+        RHI.C0 = env.animation_joint_state.RHI.C0
+        RHI.C1 = env.animation_joint_state.RHI.C1
+        LHI.C0 = env.animation_joint_state.LHI.C0
+        LHI.C1 = env.animation_joint_state.LHI.C1
+        RSH.C0 = env.animation_joint_state.RSH.C0
+        RSH.C1 = env.animation_joint_state.RSH.C1
+        LSH.C0 = env.animation_joint_state.LSH.C0
+        LSH.C1 = env.animation_joint_state.LSH.C1
+        RootJoint.C0 = env.animation_joint_state.RootJoint.C0
+        RootJoint.C1 = env.animation_joint_state.RootJoint.C1
+    else
+        env.animation_joint_state = {
+            RHI = {C0 = RHI.C0, C1 = RHI.C1},
+            LHI = {C0 = LHI.C0, C1 = LHI.C1},
+            RSH = {C0 = RSH.C0, C1 = RSH.C1},
+            LSH = {C0 = LSH.C0, C1 = LSH.C1},
+            Neck = {C0 = Torso.Neck.C0, C1 = Torso.Neck.C1},
+            RootJoint = {C0 = RootJoint.C0, C1 = RootJoint.C1}
+        }
+        env.animation_sine = 0
+    end
+
+    local cf = CFrame.new
+    local angles = CFrame.Angles
+    local ROOTC0 =  RootJoint.C0
+    local NECKC0 = Torso.Neck.C0
+    local COS = math.cos
+    local ACOS = math.acos
+    local SIN = math.sin
+    local ASIN = math.asin
+    local ABS = math.abs
+    local MRANDOM = math.random
+    local FLOOR = math.floor
+    local cf=CFrame.new
+    local cn=CFrame.new
+    local euler=CFrame.fromEulerAnglesXYZ
+    local angles=CFrame.Angles
+    local reset_c0 = false
+    env.animation_ev = RunService.RenderStepped:Connect(function()
+        if not env.CustomAnimation then
+            if not reset_c0 then
+                reset_c0 = true
+                RHI.C0 = env.animation_joint_state.RHI.C0
+                LHI.C0 = env.animation_joint_state.LHI.C0
+                RSH.C0 = env.animation_joint_state.RSH.C0
+                LSH.C0 = env.animation_joint_state.LSH.C0
+                Torso.Neck.C0 = env.animation_joint_state.Neck.C0
+                RootJoint.C0 = env.animation_joint_state.RootJoint.C0
+            end
+            return
+        end
+        local sine = env.animation_sine
+        local hum = Character:FindFirstChildOfClass("Humanoid")
+        if hum then
+            local animator = hum:FindFirstChild("Animator")
+            if animator then
+                for i, track in ipairs(animator:GetPlayingAnimationTracks()) do
+                    track:Stop(0)
+                end
+            end
+        end
+
+        local torvel=(rootpart.Velocity*Vector3.new(1,0,1)).magnitude
+        if torvel<10 then
+            --idle
+            RHI.C0=Clerp(RHI.C0,cf(1,-0.25,-0.5)*angles(math.rad(0),math.rad(90),math.rad(0))*angles(math.rad(-2.5),math.rad(0),math.rad(-10)),.1)
+            LHI.C0=Clerp(LHI.C0,cf(-1,-1,0)*angles(math.rad(0),math.rad(-90),math.rad(0))*angles(math.rad(-2.5),math.rad(0),math.rad(10)),.1)
+            RootJoint.C0=Clerp(RootJoint.C0,RootCF*cf(0,0,1.5 + 0.1 * math.cos(sine / 28))*angles(math.rad(0 - 1 * math.cos(sine / 34)),math.rad(0),math.rad(0)),.1)
+            Torso.Neck.C0=Clerp(Torso.Neck.C0,necko*angles(math.rad(15),math.rad(0),math.rad(0)),.1)
+            RSH.C0=Clerp(RSH.C0,cf(1.5,0.5,0)*angles(math.rad(10),math.rad(0),math.rad(20 + 2.5 * math.cos(sine / 28))),.1)
+            LSH.C0=Clerp(LSH.C0,cf(-1.5,0.5,0)*angles(math.rad(10),math.rad(0),math.rad(-20 - 2.5 * math.cos(sine / 28))),.1)
+        elseif torvel>10 then
+            --Walk/Running
+            RHI.C0=Clerp(RHI.C0,cf(1,-0.25,-0.5)*angles(math.rad(0),math.rad(90),math.rad(0))*angles(math.rad(-2.5),math.rad(0),math.rad(-20)),.2)
+            LHI.C0=Clerp(LHI.C0,cf(-1,-1,0)*angles(math.rad(0),math.rad(-90),math.rad(0))*angles(math.rad(-2.5),math.rad(0),math.rad(20)),.2)
+            RootJoint.C0=Clerp(RootJoint.C0,RootCF*cf(0,-0.5,0.5 + 0.1 * math.cos(sine / 28))*angles(math.rad(75),math.rad(0),math.rad(0)),.2)
+            Torso.Neck.C0=Clerp(Torso.Neck.C0,necko*angles(math.rad(-45),math.rad(0),math.rad(0)),.2)
+            RSH.C0=Clerp(RSH.C0,cf(1.5,0.5,0)*angles(math.rad(-30),math.rad(0),math.rad(30 + 2.5 * math.cos(sine / 28))),.2)
+            LSH.C0=Clerp(LSH.C0,cf(-1.5,0.5,0)*angles(math.rad(-30),math.rad(0),math.rad(-30 - 2.5 * math.cos(sine / 28))),.2)
+        end
+        reset_c0 = false
+        env.animation_sine = sine + 1
+    end)
+end
