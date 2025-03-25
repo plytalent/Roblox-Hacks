24614708270cb97163dcf2a81d31a9ef5c772237
plytalent <39195826+plytalent@users.noreply.github.com>
2025-03-25 16:12:18 +0700

    Update Fluent ManaStuff.lua

--- Fluent ManaStuff.lua
+++ Fluent ManaStuff.lua
@@ -14,63 +14,106 @@ LocalPlayer.CharacterAdded:Connect(function(newcharacter)
     cf_of_hrp = Character:GetPrimaryPartCFrame()
     rootpart = Character.HumanoidRootPart
 end)
-
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+if RunService:IsStudio() then
+    Fluent = require(game:GetService("ReplicatedStorage").Fluent)
+else
+    Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+    SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+    InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+end
 local getgenv = getgenv or function() if not _G.getgenv then print("Create getgenv Tables")_G.getgenv = {} end return _G.getgenv end
 local env = getgenv()
-local Drawing_Exist = pcall(function() Drawing.new("Text"):Remove() end)
+local Drawing_Exist, _ = pcall(function() Drawing.new("Text"):Remove() return nil end)
+
 function worldpoint_to_viewpoint(pos)
     local pos, in_fov = workspace.CurrentCamera:WorldToViewportPoint(pos)
     return {Vector2.new(pos.X,pos.Y),in_fov}
 end
-if Drawing_Exist then
-    function synTextLabel()
-        if not env._Drawing then
-            env._Drawing = {}
-        end
-        local TextLabel = Drawing.new("Text")
-        env._Drawing[#env._Drawing+1] = TextLabel
-        TextLabel.Text = "Place"
-        TextLabel.Size = 24
-        TextLabel.Center = true
-        TextLabel.Outline = true
-        TextLabel.Color = Color3.new(255/255, 0/255, 0/255)
-        TextLabel.OutlineColor = Color3.new(0, 0, 0)
-        return TextLabel
+if not Drawing_Exist then
+    local ProtectGui = protectgui or (syn and syn.protect_gui) or function() warn("protect_gui No Exist!") end
+    Drawing = env._fake_Drawing
+    if not Drawing then
+        Drawing = {}
+        Drawing._objs = {}
+        Drawing._update_loop = RunService.RenderStepped:Connect(function()
+            for property, _internal_obj in pairs(Drawing._objs) do
+                if not property._removed then
+                    _internal_obj.Text = property["Text"]
+                    _internal_obj.Visible = property["Visible"]
+                    _internal_obj.TextSize = property["Size"]
+                    _internal_obj.TextTransparency = property["Transparency"]
+                    _internal_obj.TextStrokeTransparency = property["Outline"] and 0 or 1
+                    _internal_obj.Position = UDim2.new(0,property["Position"].X,0,property["Position"].Y)
+                    property.TextBounds = _internal_obj.TextBounds
+                end
+            end
+        end)
+        Drawing._gui = Instance.new("ScreenGui", RunService:IsStudio() and LocalPlayer.PlayerGui or game:GetService("CoreGui"))
+        ProtectGui(Drawing._gui)
+        Drawing._gui.Name = "Fake Drawing"
+        Drawing._fake = true
     end
-    local Visualize = env.Visualize
-    env.Enable_Visualize = false
-    if not Visualize then 
-        Visualize = synTextLabel()
-        env.Visualize = Visualize
-    else
-        RunService:UnbindFromRenderStep("Visualize CF_hrp_point")
-    end     
-    RunService:BindToRenderStep("Visualize CF_hrp_point", 0,function()
-        local cf_p = cf_of_hrp.p
-        local point_fov = worldpoint_to_viewpoint(cf_p)
-        Visualize.Text = string.format("Current Point: [X: %.2f][Y: %.2f][Z: %.2f]",cf_p.X,cf_p.Y,cf_p.Z)
-        Visualize.Position = pos_and_infov[1]
-        Visualize.Visible = in_fov[2] and env.Enable_Visualize
-    end)
-else
-    function synTextLabel()
-        if not env._Drawing then
-            env._Drawing = {}
+    function Drawing.new(Type)
+        if Type ~= "Text" then
+            error(string.format("s% not Supported", Type))
+            return
         end
-        local TextLabel = {}
-        env._Drawing[#env._Drawing+1] = TextLabel
-        TextLabel.Text = "Place"
-        TextLabel.Size = 24
-        TextLabel.Center = true
-        TextLabel.Outline = true
-        TextLabel.Color = Color3.new(255/255, 0/255, 0/255)
-        TextLabel.OutlineColor = Color3.new(0, 0, 0)
-        return TextLabel
+        local _internal_obj = Instance.new("TextLabel",Drawing._gui)
+        _internal_obj.BackgroundTransparency = 1
+        local obj_property = {
+            Font = 0,
+            Size = 24,
+            Text = "Place",
+            Color = Color3.new(255/255, 0/255, 0/255),
+            Center = true,
+            Outline = true,
+            _removed = false,
+            Position = Vector2.new(0, 0),
+            TextBounds = _internal_obj.TextBounds,
+            Transparency = 0,
+            OutlineColor = Color3.new(0, 0, 0)
+        }
+        function obj_property:Remove()
+            obj_property._removed = true
+            _internal_obj:Destory()
+        end
+        Drawing._objs[obj_property] = _internal_obj
+        return obj_property
     end
+    if not env._fake_Drawing then
+        env._fake_Drawing = Drawing
+    end
+end   
+function DrawTextLabel()
+    if not env._Drawing then
+        env._Drawing = {}
+    end
+    local TextLabel = Drawing.new("Text")
+    env._Drawing[#env._Drawing+1] = TextLabel
+    TextLabel.Text = "Place"
+    TextLabel.Size = 24
+    TextLabel.Center = true
+    TextLabel.Outline = true
+    TextLabel.Color = Color3.new(255/255, 0/255, 0/255)
+    TextLabel.OutlineColor = Color3.new(0, 0, 0)
+    return TextLabel
 end
+local Visualize = env.Visualize
+env.Enable_Visualize = false
+if not Visualize then 
+    Visualize = DrawTextLabel()
+    env.Visualize = Visualize
+else
+    RunService:UnbindFromRenderStep("Visualize CF_hrp_point")
+end     
+RunService:BindToRenderStep("Visualize CF_hrp_point", 0,function()
+    local cf_p = cf_of_hrp.p
+    local point_fov = worldpoint_to_viewpoint(cf_p)
+    Visualize.Text = string.format("Current Point: [X: %.2f][Y: %.2f][Z: %.2f]",cf_p.X,cf_p.Y,cf_p.Z)
+    Visualize.Position = point_fov[1]
+    Visualize.Visible = point_fov[2] and env.Enable_Visualize
+end)
+
 local Window = Fluent:CreateWindow({
     Title = "ManaStuff",
     SubTitle = "by RubyTheSilent",
@@ -84,13 +127,12 @@ local Window = Fluent:CreateWindow({
 local Tabs = {
     Main = Window:AddTab({ Title = "Movements", Icon = "chevrons-up" }),
     Keybinds = Window:AddTab({ Title = "Keybinds", Icon = "keyboard" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
+    Debug = Window:AddTab({ Title = "Debug", Icon = "bug" })
 }
 
 local movement_dir_z, movement_dir_x = 0, 0
-
 local Options = Fluent.Options
-
 local ManaSoulToggle = Tabs.Main:AddToggle("NoClip", {
     Title = "ManaSoul",
     Default = false
@@ -128,16 +170,11 @@ local ManaSpeed = Tabs.Main:AddSlider("MovementSpeed", {
         end
     end
 })
-
-local MovementModeDropdown = Tabs.Main:AddDropdown("MovementMode",
+local MovementModeDropdown = Tabs.Main:AddDropdown("MovementMode",{
     Title = "ManaFly/Run Type",
     Values = {"Positioning", "Velocity"},
-    Default = {"Positioning"},
-)
-MovementModeDropdown:OnChanged(function(Value)
-    env.MovementMode = Value
-end)
-
+    Default = "Positioning",
+})
 local ManaSoulKey = Tabs.Keybinds:AddKeybind("ManaSoulKeyBind", {
     Title = "ManaSoul Key",
     Mode = "Toggle",
@@ -153,6 +190,17 @@ local ManaRunKey = Tabs.Keybinds:AddKeybind("ManaFlyKeyBind", {
     Mode = "Toggle",
     Default = "M",
 })
+local cf_of_hrp_visualize_toggle = Tabs.Debug:AddToggle("cf_of_hrp_visualize", {
+    Title = "Point Visual",
+    Default = false
+})
+cf_of_hrp_visualize_toggle:OnChanged(function()
+    env.Enable_Visualize = Options.cf_of_hrp_visualize.Value
+end)
+Options.cf_of_hrp_visualize:SetValue(env.Enable_Visualize)
+MovementModeDropdown:OnChanged(function(Value)
+    env.MovementMode = Value
+end)
 
 ManaFlyToggle:OnChanged(function()
     if Character.PrimaryPart then
@@ -166,6 +214,7 @@ end)
 
 ManaFlyKey:OnClick(function()
     Options.Fly:SetValue(not Options.Fly.Value)
+    Options.Speed:SetValue(false)
     if Character.PrimaryPart then
         cf_of_hrp = Character:GetPrimaryPartCFrame()
     end
@@ -186,6 +235,10 @@ ManaFlyKey:OnClick(function()
 end)
 
 ManaRunKey:OnClick(function()
+    if Options.Fly.Value then
+        Options.Speed:SetValue(false)
+        return
+    end
     Options.Speed:SetValue(not Options.Speed.Value)
     movement_dir_z, movement_dir_x = 0, 0
     local change = (Options.MovementSpeed.Value / 100)
@@ -225,48 +278,47 @@ local leftorright,backorforward = 0,0
 local targetCF
 local anti_grav = 2.9
 RunService.Stepped:Connect(function()
-	local _delta = tick()-delta
-	delta = tick()
-	if math.abs((cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y) > 0.0001 then
-		print((cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y)
-		anti_grav = (cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y
-	end
-	if _delta >= (1/60)*0.99 or env.MovementMode == "Velocity" then
-		if Options.Fly.Value or Options.Speed.Value then
-			local rx,ry,rz = workspace.CurrentCamera.CFrame:ToOrientation()
-			targetCF = CFrame.new(0,0,0)*CFrame.Angles(0,ry,0)
-			if Options.Fly.Value then
-				targetCF = targetCF * CFrame.Angles(rx,0,0)
-			end
-			if Options.Speed.Value and env.MovementMode == "Positioning" and Character.PrimaryPart then
-				cf_of_hrp = Character:GetPrimaryPartCFrame()
-			end
-			cf_of_hrp = (CFrame.new(cf_of_hrp.p) * targetCF)*CFrame.new(movement_dir_x, 0, movement_dir_z)
-			if Character ~= nil then
-				if env.MovementMode == "Positioning" and Character.PrimaryPart then
-					Character:SetPrimaryPartCFrame(cf_of_hrp)
-					if Options.Fly.Value then
-						rootpart.Velocity = Vector3.new(0,0,0)
-					end
-				elseif env.MovementMode == "Velocity" then
-					local Intensity = (rootpart.Position - cf_of_hrp.Position).Magnitude
-					local lk = Vector3.new(0,0,0)
-					if Intensity < 10 then
-						lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*10)
-					else
-						lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*Intensity)
-					end
-					
-					local target_velocity = lk
-					local yv = target_velocity.Y + anti_grav
-					if Options.Speed.Value then
-						yv = rootpart.Velocity.y
-					end
-					rootpart.Velocity = Vector3.new(target_velocity.X, yv , target_velocity.Z)
-				end
-			end
-		end
-	end
+    local _delta = tick()-delta
+    delta = tick()
+    if math.abs((cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y) > 0.0001 then
+        anti_grav = (cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y
+    end
+    if _delta >= (1/60)*0.99 or env.MovementMode == "Velocity" then
+        if Options.Fly.Value or Options.Speed.Value then
+            local rx,ry,rz = workspace.CurrentCamera.CFrame:ToOrientation()
+            targetCF = CFrame.new(0,0,0)*CFrame.Angles(0,ry,0)
+            if Options.Fly.Value then
+                targetCF = targetCF * CFrame.Angles(rx,0,0)
+            end
+            if Options.Speed.Value and env.MovementMode == "Positioning" and Character.PrimaryPart then
+                cf_of_hrp = Character:GetPrimaryPartCFrame()
+            end
+            cf_of_hrp = (CFrame.new(cf_of_hrp.p) * targetCF)*CFrame.new(movement_dir_x, 0, movement_dir_z)
+            if Character ~= nil then
+                if env.MovementMode == "Positioning" and Character.PrimaryPart then
+                    Character:SetPrimaryPartCFrame(cf_of_hrp)
+                    if Options.Fly.Value then
+                        rootpart.Velocity = Vector3.new(0,0,0)
+                    end
+                elseif env.MovementMode == "Velocity" then
+                    local Intensity = (rootpart.Position - cf_of_hrp.Position).Magnitude
+                    local lk = Vector3.new(0,0,0)
+                    if Intensity < 10 then
+                        lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*10)
+                    else
+                        lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*Intensity)
+                    end
+
+                    local target_velocity = lk
+                    local yv = target_velocity.Y + anti_grav
+                    if Options.Speed.Value then
+                        yv = rootpart.Velocity.y
+                    end
+                    rootpart.Velocity = Vector3.new(target_velocity.X, yv , target_velocity.Z)
+                end
+            end
+        end
+    end
 end)
 UIS.InputBegan:Connect(function(input,gameProcessed)
     local k = input.KeyCode.Name:lower()
@@ -300,77 +352,91 @@ Fluent:Notify({
     Content = "The Main Script has been loaded.",
     Duration = 8
 })
-
-Fluent:Notify({
-    Title = "SubModule",
-    Content = "Try To Load SubModule",
-    Duration = 8
-})
-local s, err =  pcall(function()
-    local map_place_name_id = HttpService:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/plytalent/Roblox-Hacks/refs/heads/main/Modules/map.json"))
-    local placename = ""
-    for map_name, array_of_ids in pairs(map_place_name_id) do
-        print(map_name,array_of_ids)
-        if table.find(array_of_ids,tostring(game.PlaceId)) then
-            placename = map_name
-            break
+if not RunService:IsStudio() then
+    Fluent:Notify({
+        Title = "SubModule",
+        Content = "Try To Load SubModule",
+        Duration = 8
+    })
+    local s, err =  pcall(function()
+        local map_place_name_id = HttpService:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/plytalent/Roblox-Hacks/refs/heads/main/Modules/map.json"))
+        local placename = ""
+        for map_name, array_of_ids in pairs(map_place_name_id) do
+            print(map_name,array_of_ids)
+            if table.find(array_of_ids,tostring(game.PlaceId)) then
+                placename = map_name
+                break
+            end
         end
-    end
-    if placename ~= "" then
-        sub_module_source = game:HttpGet("https://raw.githubusercontent.com/plytalent/Roblox-Hacks/refs/heads/main/Modules/"..placename.."_module.lua")
-        local _s, sub_module_init = pcall(loadstring(sub_module_source))
-        if _s then
-            if sub_module_init then 
-                local _s, e = pcall(sub_module_init,HttpService, RunService, UIS, Players, Fluent, Options, SaveManager, InterfaceManager, Window, Tabs, getgenv())
-                if _s then
-                    Fluent:Notify({
-                        Title = "SubModule",
-                        Content = "SubModule Has Been Loaded",
-                        Duration = 8
-                    })
+        if placename ~= "" then
+            sub_module_source = game:HttpGet("https://raw.githubusercontent.com/plytalent/Roblox-Hacks/refs/heads/main/Modules/"..placename.."_module.lua")
+            local _s, sub_module_init = pcall(loadstring(sub_module_source))
+            if _s then
+                if sub_module_init then 
+                    local _s, e = pcall(sub_module_init,HttpService, RunService, UIS, Players, Fluent, Options, SaveManager, InterfaceManager, Window, Tabs, getgenv())
+                    if _s then
+                        Fluent:Notify({
+                            Title = "SubModule",
+                            Content = "SubModule Has Been Loaded",
+                            Duration = 8
+                        })
+                    else
+                        print(e)
+                        Fluent:Notify({
+                            Title = "SubModule",
+                            Content = "Internal Error",
+                            Duration = 8
+                        })
+                    end
                 else
-                    print(e)
                     Fluent:Notify({
                         Title = "SubModule",
-                        Content = "Internal Error",
+                        Content = "Missing Init Function",
                         Duration = 8
                     })
                 end
             else
-                Fluent:Notify({
-                    Title = "SubModule",
-                    Content = "Missing Init Function",
-                    Duration = 8
-                })
+                print(sub_module_init)
             end
         else
-            print(sub_module_init)
+            Fluent:Notify({
+                Title = "SubModule",
+                Content = "No SubModule Has Been Found For This Place",
+                SubContent = "PlaceID: "..tostring(game.PlaceId),
+                Duration = 8
+            })
         end
-    else
+    end)
+    if not s then
+        print(err)
         Fluent:Notify({
             Title = "SubModule",
-            Content = "No SubModule Has Been Found For This Place",
-            SubContent = "PlaceID: "..tostring(game.PlaceId),
+            Content = "Failed To Load SubModule",
+            SubContent = err,
             Duration = 8
         })
     end
-end)
-if not s then
-    print(err)
+else
     Fluent:Notify({
         Title = "SubModule",
-        Content = "Failed To Load SubModule",
-        SubContent = err,
+        Content = "Not Available",
         Duration = 8
     })
 end
-SaveManager:SetLibrary(Fluent)
-InterfaceManager:SetLibrary(Fluent)
-SaveManager:IgnoreThemeSettings()
-SaveManager:SetIgnoreIndexes({})
-InterfaceManager:SetFolder("Manastuff Hub")
-SaveManager:SetFolder("Manastuff Hub/"..tostring(game.PlaceId))
-InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-SaveManager:BuildConfigSection(Tabs.Settings)
+if SaveManager and InterfaceManager then
+    SaveManager:SetLibrary(Fluent)
+    InterfaceManager:SetLibrary(Fluent)
+    SaveManager:IgnoreThemeSettings()
+    SaveManager:SetIgnoreIndexes({})
+    InterfaceManager:SetFolder("Manastuff Hub")
+    SaveManager:SetFolder("Manastuff Hub/"..tostring(game.PlaceId))
+    InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+    SaveManager:BuildConfigSection(Tabs.Settings)
+    SaveManager:LoadAutoloadConfig()
+else
+    Tabs.Settings:AddParagraph({
+        Title = "Missing Library",
+        Content = "Can't Use Save Manager And Interface Manager"
+    })
+end
 Window:SelectTab(1)
-SaveManager:LoadAutoloadConfig()
