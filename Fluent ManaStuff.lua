706f6b93385a7c7f4690b11f4b31ee0b50a2fffc
plytalent <39195826+plytalent@users.noreply.github.com>
2025-04-02 10:08:42 +0700

    Update Fluent ManaStuff.lua
    Move Movement Fly And Run To Sub Module

--- Fluent ManaStuff.lua
+++ Fluent ManaStuff.lua
@@ -150,51 +150,10 @@ local ManaSoulToggle = Tabs.Main:AddToggle("NoClip", {
     Title = "ManaSoul",
     Default = false
 })
-local ManaFlyToggle = Tabs.Main:AddToggle("Fly", {
-    Title = "ManaFly",
-    SubTitle = "(In Gaia)",
-    Default = false
-})
-local ManaRunToggle = Tabs.Main:AddToggle("Speed", {
-    Title = "ManaRun",
-    Default = false
-})
-local ManaSpeed = Tabs.Main:AddSlider("MovementSpeed", {
-    Title = "Mana Speed",
-    Description = "Speed For Both ManaFly/Run",
-    Default = 250,
-    Min = 0,
-    Max = 1000,
-    Rounding = 1,
-    Callback = function(Value)
-        local change = Value/100
-        local change = (Options.MovementSpeed.Value / 100)
-        if UIS:IsKeyDown(Enum.KeyCode.S) or UIS:IsKeyDown(Enum.KeyCode.W) or UIS:IsKeyDown(Enum.KeyCode.D) or UIS:IsKeyDown(Enum.KeyCode.A) then
-            local new_vector = control_module:GetMoveVector() * change
-            movement_dir_x = new_vector.x
-            movement_dir_z = new_vector.z
-        end
-    end
-})
-local MovementModeDropdown = Tabs.Main:AddDropdown("MovementMode",{
-    Title = "ManaFly/Run Type",
-    Values = {"Positioning", "Velocity", "BodyMover(Velocity)", "BodyMover(Position)", "AlignPosition"},
-    Default = "Positioning",
-})
 local ManaSoulKey = Tabs.Keybinds:AddKeybind("ManaSoulKeyBind", {
     Title = "ManaSoul Key",
     Mode = "Toggle",
     Default = "P",
-})
-local ManaFlyKey = Tabs.Keybinds:AddKeybind("ManaFlyKeyBind", {
-    Title = "ManaFly Key",
-    Mode = "Toggle",
-    Default = "K",
-})
-local ManaRunKey = Tabs.Keybinds:AddKeybind("ManaFlyKeyBind", {
-    Title = "ManaRun Key",
-    Mode = "Toggle",
-    Default = "M",
 })
 local cf_of_hrp_visualize_toggle = Tabs.Debug:AddToggle("cf_of_hrp_visualize", {
     Title = "Point Visual",
@@ -204,48 +163,11 @@ cf_of_hrp_visualize_toggle:OnChanged(function()
     env.Enable_Visualize = Options.cf_of_hrp_visualize.Value
 end)
 Options.cf_of_hrp_visualize:SetValue(env.Enable_Visualize)
-MovementModeDropdown:OnChanged(function(Value)
-    env.MovementMode = Value
-end)
-
-ManaFlyToggle:OnChanged(function()
-    if Character.PrimaryPart then
-        cf_of_hrp = Character:GetPrimaryPartCFrame()
-    end
-end)
 
 ManaSoulKey:OnClick(function()
     Options.NoClip:SetValue(not Options.NoClip.Value)
 end)
 
-ManaFlyKey:OnClick(function()
-    Options.Fly:SetValue(not Options.Fly.Value)
-    Options.Speed:SetValue(false)
-    if Character.PrimaryPart then
-        cf_of_hrp = Character:GetPrimaryPartCFrame()
-    end
-    local change = (Options.MovementSpeed.Value / 100)
-    if UIS:IsKeyDown(Enum.KeyCode.S) or UIS:IsKeyDown(Enum.KeyCode.W) or UIS:IsKeyDown(Enum.KeyCode.D) or UIS:IsKeyDown(Enum.KeyCode.A) then
-        local new_vector = control_module:GetMoveVector() * change
-        movement_dir_x = new_vector.x
-        movement_dir_z = new_vector.z
-    end
-end)
-
-ManaRunKey:OnClick(function()
-    if Options.Fly.Value then
-        Options.Speed:SetValue(false)
-        return
-    end
-    Options.Speed:SetValue(not Options.Speed.Value)
-    movement_dir_z, movement_dir_x = 0, 0
-    local change = (Options.MovementSpeed.Value / 100)
-    if UIS:IsKeyDown(Enum.KeyCode.S) or UIS:IsKeyDown(Enum.KeyCode.W) or UIS:IsKeyDown(Enum.KeyCode.D) or UIS:IsKeyDown(Enum.KeyCode.A) then
-        local new_vector = control_module:GetMoveVector() * change
-        movement_dir_x = new_vector.x
-        movement_dir_z = new_vector.z
-    end
-end)
 env.Manastuff_NoClip_EV = RunService.Stepped:Connect(function()
     if Options.NoClip.Value then
         if Character then
@@ -257,144 +179,88 @@ env.Manastuff_NoClip_EV = RunService.Stepped:Connect(function()
         end
     end
 end)
-function env.InputBegan_Callback(input,gameProcessed)
-    --// dummy Function
-end
-function env.InputEnded_Callback(input,gameProcessed)
-    --// dummy Function
-end
---// Variables
-local delta = tick()
-local leftorright,backorforward = 0,0
-local targetCF
-local anti_grav = 2.9
-RunService.Stepped:Connect(function()
-    local _delta = tick()-delta
-    delta = tick()
-    if math.abs((cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y) > 0.0001 then
-        anti_grav = (cf_of_hrp.Position.Y+1.45) - rootpart.Position.Y
-    end
-    if not env.BodyVelocity then
-        env.BodyVelocity = Instance.new("BodyVelocity")
-    end
-    if not env.BodyPosition then
-        env.BodyPosition = Instance.new("BodyPosition")
-    end
-    if not env.AlignPosition then
-        env.AlignPosition = Instance.new("AlignPosition")
-        env.AlignPosition_At = Instance.new("Attachment")
-    end
-    if (Options.Fly.Value or Options.Speed.Value) and env.MovementMode == "BodyMover(Velocity)" then
-        env.BodyVelocity.Parent = rootpart
-    else
-        env.BodyVelocity.Parent = nil
-    end
-    if (Options.Fly.Value or Options.Speed.Value) and env.MovementMode == "BodyMover(Position)" then
-        env.BodyPosition.Parent = rootpart
-    else
-        env.BodyPosition.Parent = nil
-    end
-    if (Options.Fly.Value or Options.Speed.Value) and env.MovementMode == "AlignPosition" then
-        env.AlignPosition.Parent = rootpart
-        env.AlignPosition_At.Parent = rootpart
-    else
-        env.AlignPosition.Parent = nil
-        env.AlignPosition_At.Parent = nil
-    end
-    if _delta >= (1/60)*0.99 or env.MovementMode == "Velocity" then
-        if Options.Fly.Value or Options.Speed.Value then
-            local rx,ry,rz = workspace.CurrentCamera.CFrame:ToOrientation()
-            targetCF = CFrame.new(0,0,0)*CFrame.Angles(0,ry,0)
-            if Options.Fly.Value then
-                targetCF = targetCF * CFrame.Angles(rx,0,0)
-            end
-            if Options.Speed.Value and env.MovementMode == "Positioning" and Character.PrimaryPart then
-                cf_of_hrp = Character:GetPrimaryPartCFrame()
-            end
-            cf_of_hrp = (CFrame.new(cf_of_hrp.p) * targetCF)*CFrame.new(movement_dir_x, 0, movement_dir_z)
-            if Character ~= nil then
-                if env.MovementMode == "Positioning" and Character.PrimaryPart then
-                    Character:SetPrimaryPartCFrame(cf_of_hrp)
-                    if Options.Fly.Value then
-                        rootpart.Velocity = Vector3.new(0,0,0)
-                    end
-                elseif env.MovementMode == "Velocity" then
-                    local Intensity = (rootpart.Position - cf_of_hrp.Position).Magnitude
-                    local lk = Vector3.new(0,0,0)
-                    if Intensity < 10 then
-                        lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*10)
-                    else
-                        lk = (cf_of_hrp.Position - rootpart.Position).Unit*(Intensity*Intensity)
-                    end
-
-                    local target_velocity = lk
-                    local yv = target_velocity.Y + anti_grav
-                    if Options.Speed.Value then
-                        yv = rootpart.Velocity.y
-                    end
-                    rootpart.Velocity = Vector3.new(target_velocity.X, yv , target_velocity.Z)
-                elseif env.MovementMode == "BodyMover(Velocity)" then
-                    cf_of_hrp = Character:GetPrimaryPartCFrame()
-                    local change = (Options.MovementSpeed.Value/1.5)
-                    if Options.Fly.Value then
-                        env.BodyVelocity.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
-                    else
-                        env.BodyVelocity.MaxForce = Vector3.new(math.huge, 0, math.huge)
-                    end
-                    env.BodyVelocity.Velocity = workspace.CurrentCamera.CFrame:VectorToWorldSpace(control_module:GetMoveVector() * change)
-                elseif env.MovementMode == "BodyMover(Position)" then
-                    if Options.Fly.Value then
-                        env.BodyPosition.MaxForce = Vector3.new(math.huge, math.huge, math.huge)
-                    else
-                        env.BodyPosition.MaxForce = Vector3.new(math.huge, 0, math.huge)
-                    end
-                    env.BodyPosition.D = 300
-                    env.BodyPosition.P = 10000
-                    env.BodyPosition.Position = cf_of_hrp.p
-                elseif env.MovementMode == "AlignPosition" then
-                    if Options.Fly.Value then
-                        env.AlignPosition.ForceLimitMode = Enum.ForceLimitMode.Magnitude
-                        env.AlignPosition.MaxForce = math.huge
-                    else
-                        env.AlignPosition.ForceLimitMode = Enum.ForceLimitMode.PerAxis
-                        env.AlignPosition.MaxAxesForce = Vector3.new(math.huge, 0, math.huge)
-                    end
-                    env.AlignPosition.Mode = Enum.PositionAlignmentMode.OneAttachment
-                    env.AlignPosition.ApplyAtCenterOfMass = true
-                    env.AlignPosition.Responsiveness = 75
-                    env.AlignPosition.Position = cf_of_hrp.p					
-                    env.AlignPosition.Attachment0 = env.AlignPosition_At
-                end
-            end
-        end
-    end
-end)
-UIS.InputBegan:Connect(function(input,gameProcessed)
-    local k = input.KeyCode.Name:lower()
-    if k == "w" or k == "s" or k == "a" or k == "d" then
-        local change = (Options.MovementSpeed.Value / 100)
-        local new_vector = control_module:GetMoveVector() * change
-        movement_dir_x = new_vector.x
-        movement_dir_z = new_vector.z
-    end
-    env.InputBegan_Callback(input,gameProcessed)
-end)
-UIS.InputEnded:Connect(function(input,gameProcessed)
-    local k = input.KeyCode.Name:lower()
-    if k == "w" or k == "s" or k == "a" or k == "d" then
-        local change = (Options.MovementSpeed.Value / 100)
-        local new_vector = control_module:GetMoveVector() * change
-        movement_dir_x = new_vector.x
-        movement_dir_z = new_vector.z
-    end
-    env.InputEnded_Callback(input,gameProcessed)
-end)
 
 Fluent:Notify({
     Title = "ManaStuff MainModule Loaded",
     Content = "The Main Script has been loaded.",
     Duration = 8
 })
+
+local Movment_Sub_module
+if not RunService:IsStudio() then
+    Fluent:Notify({
+        Title = "MainModule",
+        Content = "Try To Load Movement Module",
+        Duration = 8
+    })
+    local s, err =  pcall(function()
+        local sub_module_source = game:HttpGet("https://raw.githubusercontent.com/plytalent/Roblox-Hacks/refs/heads/main/Modules/_Movement_Module.lua")
+        local _s, sub_module_init = pcall(loadstring(sub_module_source))
+        if _s then
+            local _s, e = pcall(
+                sub_module_init,
+                {
+                    Fluent
+                },
+                Tabs.Main,
+                {
+                    Title = "ManaFly",
+                    Description = "(In Gaia)",
+                    Default = false
+                },
+                {
+                    Title = "ManaRun",
+                    Default = false
+                },{
+                    Title = "Speed",
+                    Description = "Speed For Both ManaFly/ManaRun",
+                    Default = 250,
+                    Min = 0,
+                    Max = 500,
+                    Rounding = 1
+                }
+            )
+            if _s then
+                Fluent:Notify({
+                    Title = "MainModule",
+                    Content = "Loaded Movement Submodule",
+                    Duration = 8
+                })
+            else
+                print(e)
+                Fluent:Notify({
+                    Title = "Movement Module",
+                    Content = "Internal Error",
+                    Duration = 8
+                })
+            end
+        else
+            print(sub_module_init)
+            Fluent:Notify({
+                Title = "Movement Module",
+                Content = "Module Error",
+                Duration = 8
+            })
+        end
+    end)
+    if not s then
+        print(err)
+        Fluent:Notify({
+            Title = "MainModule",
+            Content = "Failed To Load Movement SubModule",
+            SubContent = err,
+            Duration = 8
+        })
+    end
+else
+    Movment_Sub_module = require(game:GetService("ReplicatedStorage").Sub_module.Movment)
+    Fluent:Notify({
+        Title = "MainModule",
+        Content = "Loaded Local Movement Submodule",
+        Duration = 8
+    })
+end
+
 if not RunService:IsStudio() then
     Fluent:Notify({
         Title = "SubModule",
